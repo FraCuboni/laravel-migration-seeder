404c794edfbb49acd1283a4ade22af3083f2bbde
FraCuboni <fracuboni@gmail.com>
2024-09-13 16:30:25 +0200

    feat having problems with train seeder migration

---
+++ database/seeders/TrainSeeder.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Train;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Faker\Generator as Faker;
+use Illuminate\Database\Seeder;
+
+class TrainSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(Faker $faker): void
+    {
+        for ($i = 0; $i < 20; $i++) {
+            $train = new Train();
+
+            $train->compagnia = $faker->randomElement(['italo', 'trenitalia', 'trenord']);
+            $train->stazione_partenza = $faker->randomElement(['Roma termini', 'Torino Porta nuova', 'Torino Porta Susa', 'Milano', 'Bari', 'Aosta']);
+            $train->stazione_arrivo = $faker->randomElement(['Roma termini', 'Torino Porta nuova', 'Torino Porta Susa', 'Milano', 'Bari', 'Aosta']);
+            $train->orario_partenza = $faker->time();
+            $train->orario_arrivo = $faker->time();
+            $train->codice_treno = $faker->numberBetween(3, 10);
+            $train->numero_carrozze = $faker->numberBetween(3, 10);
+            $train->in_orario = $faker->boolean();
+            $train->cancellato = $faker->boolean();
+        }
+
+        //     $table->string('azienda');
+        //     $table->string('stazione_partenza');
+        //     $table->string('stazione_arrivo');
+        //     $table->time('orario_partenza');
+        //     $table->time('orario_arrivo');
+        //     $table->string('codice_treno')->unique();
+        //     $table->unsignedTinyInteger('numero_carrozze');
+        //     $table->boolean('in_orario')->default(true);
+        //     $table->boolean('cancellato')->default(false);
+    }
+}
