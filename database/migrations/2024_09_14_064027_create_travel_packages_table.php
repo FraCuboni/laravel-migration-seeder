c6b75c0a40605a34275e39ff66beda4e59f8977d
FraCuboni <fracuboni@gmail.com>
2024-09-14 08:52:59 +0200

    feat created table 'travel packages'

---
+++ database/migrations/2024_09_14_064027_create_travel_packages_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('travel_packages', function (Blueprint $table) {
+            $table->id();
+            $table->string('azienda');
+            $table->string('stazione_partenza');
+            $table->string('stazione_arrivo');
+            $table->string('destinazione');
+            $table->dateTime('data_partenza', $precision = 0);
+            $table->dateTime('data_arrivo', $precision = 0);
+            $table->float('prezzo', 8, 2);
+            $table->string('descrizione');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('travel_packages');
+    }
+};
